70313829b37308009e09f9c77cb2e39c6ac8a304
parameshwarrao <parameshwarrao.s@gmail.com>
2021-12-13 00:15:36 +0530

    components for blogs personal finance disclaimer and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ToolsComponent } from './tools/tools.component';
+import { PersonalFinanceComponent } from './personal-finance/personal-finance.component';
+import { BlogComponent } from './blog/blog.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ToolsComponent,
+    PersonalFinanceComponent,
+    BlogComponent,
+    DisclaimerComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent},
       {path:'about',component: AboutComponent},
+      {path:'tools',component: ToolsComponent},
+      {path:'personalFinance',component: PersonalFinanceComponent},
+      {path:'blog',component: BlogComponent},
+      {path:'DisclaimerComponent',component: DisclaimerComponent},     
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: '*', component: HomeComponent },
       { path: '**', component: HomeComponent }   
